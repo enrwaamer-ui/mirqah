bd8bffe00a6defd81e5dd3af67d5e85b11cdd28d
enrwaamer-ui <enrwaamer@gmail.com>
2026-09-15 04:19:28 +0000

    Initial commit

--- db.js
+++ db.js
@@ -1,28 +1,10 @@
-const { Pool } = require("pg");
+const { Pool } = require('pg');
 
-const pool = new Pool(
-    process.env.DATABASE_URL
-        ? {
-            connectionString: process.env.DATABASE_URL,
-            ssl: {
-                rejectUnauthorized: false
-            }
-        }
-        : {
-            user: "postgres",
-            host: "localhost",
-            database: "postgres",
-            password: "process.env.DB.PASSWORD",
-            port: 5432
-        }
-);
-
-pool.connect()
-    .then(() => {
-        console.log("Database connected successfully!");
-    })
-    .catch((err) => {
-        console.error("Database connection failed:", err.message);
-    });
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false // هذا السطر هو المطلق لتفعيل الاتصال على Render
+  }
+});
 
 module.exports = pool;
